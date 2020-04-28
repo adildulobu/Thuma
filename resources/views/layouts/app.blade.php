2ba7e0fecade3ae0dedaa936d13ad7c1b824dcdc
GS-dev123 <gsservice907@gmail.com>
2020-04-28 06:04:02 +0200

    Tela Principal feita a 50%

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+     <link rel="stylesheet" href="../css/home_style.css">
+</head>
+<body>
+@yield('navegacao')
+@yield('conteudo')
+@yield('rodape')
+
+</body>
+</html>
